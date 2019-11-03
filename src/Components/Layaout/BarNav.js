eaf710956af6d18967069779c5ac68bea1b1e9e6
Ivan-Priego <brauunsimons@gmail.com>
2019-11-02 18:11:30 -0600

    inicializando proyecto evaluacion

---
+++ src/Components/Layaout/BarNav.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class BarNav extends Component {
+  render() {
+    return (
+      <div> textInComponent </div>
+    );
+  }
+}
+export default BarNav
---
+++ src/Components/Layaout/Seccion.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Seccion extends Component {
+  render() {
+    return (
+      <div> textInComponent </div>
+    );
+  }
+}
+export default Seccion
